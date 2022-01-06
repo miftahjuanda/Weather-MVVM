8591dcd201e3df0ac2077c91ee043744aafa794b
Miftah Juanda Batubara <miftahjuanda21@gmail.com>
2022-01-06 14:59:40 +0700

    Initial Commit

---
+++ Weather-Kompas/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Weather-Kompas
+//
+//  Created by Miftah Juanda Batubara on 06/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
